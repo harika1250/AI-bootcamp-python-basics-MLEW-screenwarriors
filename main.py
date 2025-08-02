f83f2696ed1ad958f0fea12d75d0eab779a33f96
harika1250 <kandhiharika1025@gmail.com>
2025-08-02 14:58:44 +0530

    adding files

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask,request,jsonify
+import joblib 
+import numpy as np
+
+app=Flask(__name__)
+model=joblib.load('ac_model.pkl')
+
+@app.route("/")
+def home():
+    return"AC project"
+
+@app.route("/predict",methods=['post'])
+def predict():
+    data=request.json
+    temp=data.get('temperature')
+    hum=data.get('humidity')
+
+    if temp is None or hum is None:
+        return jsonify({"error":"Missing temperature or humidity"}),400 #not found status code
+    
+    prediction=model.predict(np.array([[temp,hum]]))
+    status="ON"if prediction == 1 else  "OFF"
+    return jsonify({"ac_status":status})
+if __name__=='__main__':
+    app.run(host="0.0.0.0",port=5000,debug=True)
